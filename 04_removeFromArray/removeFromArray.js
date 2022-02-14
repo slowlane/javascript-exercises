d4535972e56a524ec7cd9cd8c4ed5c10a7e2910f
Andreas Wickerfält <andreas.wickerfalt@gmail.com>
2022-02-14 20:27:04 +0100

    Next two..

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argsToRemove ) {
 
+    for(let i = 0; i < array.length; i++){
+        for(let j = 0; j < argsToRemove.length; j++){
+            if(array[i] === argsToRemove[j]){
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    if(typeof start != "number" || typeof finish != "number" ||
+    start < 0 || finish < 0){
+        return "ERROR";
+    }
+    
+    sum = 0;
 
+    if(start > finish){
+        for(let i = finish; i <= start; i++){
+            sum += i;
+        }
+    }else{
+        for(let i = start; i <= finish; i++){
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
