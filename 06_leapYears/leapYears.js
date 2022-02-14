11c704b37b740b9002ab70eaf23f7d513aeeca74
Andreas Wickerfält <andreas.wickerfalt@gmail.com>
2022-02-14 20:42:40 +0100

    Finished the rest

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(potentialLeapYear) {
+    if(potentialLeapYear % 4 === 0 &&
+        potentialLeapYear % 100 != 0 ||
+        potentialLeapYear % 400 === 0){
+       
+        return true;      
+    }
+    return false;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10; 
 };
 
-const ctof = function() {
 
+//Math.round(number * 10) / 10
+
+const ctof = function(celsius) {
+  return Math.round((celsius * 9/5 + 32) * 10) / 10; 
+  //return celsius * 9/5 + 32;
 };
 
 // Do not edit below this line
