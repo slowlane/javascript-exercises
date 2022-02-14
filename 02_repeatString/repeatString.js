f75a6f964059cac1b3b047f4bb55b154bf5fde2d
Andreas Wickerfält <andreas.wickerfalt@gmail.com>
2022-02-14 02:16:01 +0100

    FIrst few , gotta sleep

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    
+    let funcString = "";
+    
+    for(i = 0; i < num; i++){
+        funcString += string;
+    }
 
+    return funcString;
 };
 
 // Do not edit below this line
