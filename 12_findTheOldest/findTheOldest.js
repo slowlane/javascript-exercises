eb6d9fa6bdb06fae00f6eb44a58df694d4f1cf31
Andreas Wickerfält <andreas.wickerfalt@gmail.com>
2022-02-19 03:13:59 +0100

    done..

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((previous, current) => {
+        
+        if(previous["yearOfDeath"] == undefined){
+            return new Date().getFullYear() - previous["yearOfBirth"] > 
+        current["yearOfDeath"] - current["yearOfBirth"] ? previous : current;
+        } 
+        
+        else if(current["yearOfDeath"] == undefined){
+            return previous["yearOfDeath"] - previous["yearOfBirth"] > 
+            new Date().getFullYear() - current["yearOfBirth"] ? previous : current;
+        }
+        
+        return previous["yearOfDeath"] - previous["yearOfBirth"] > 
+        current["yearOfDeath"] - current["yearOfBirth"] ? previous : current;
+    }
+    );
+    
 };
 
 // Do not edit below this line
