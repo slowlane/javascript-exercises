33c4c333a78b584ac500fd411435b4d4450a6e4d
Andreas Wickerfält <andreas.wickerfalt@gmail.com>
2022-02-19 02:08:25 +0100

    gotta get used to regex huh..

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let compString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (compString
+    .split("")
+    .reverse()
+    .join("") == compString
+    );
+    
+      
 };
 
 // Do not edit below this line
