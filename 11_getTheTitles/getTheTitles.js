f1b33da9ba04e8750a41a41afa12e6b4764a298a
Andreas Wickerfält <andreas.wickerfalt@gmail.com>
2022-02-19 03:18:37 +0100

    Forgot this part!

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+    const titleArray = obj.map(book => book["title"]);
+    return titleArray;
 };
 
 // Do not edit below this line
