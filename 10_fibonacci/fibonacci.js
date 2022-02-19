ff9e03a081bffb5aa34360bba827ab87159311ca
Andreas Wickerfält <andreas.wickerfalt@gmail.com>
2022-02-19 02:35:11 +0100

    fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 0){
+        return "OOPS"
+    }
+    if(number === 0){
+        return 0;
+    }
+    
+    let fibonacci = 1;
+    let fibonacciStart = 0;
+    for(let i = 1; i < number; i++){
+        const temp = fibonacci;
+        fibonacci = fibonacci + fibonacciStart;
+        fibonacciStart = temp;
+    }
+    return fibonacci;
+    
 };
 
 // Do not edit below this line
